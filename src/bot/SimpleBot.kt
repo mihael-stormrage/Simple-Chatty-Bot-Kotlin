2d614b9cc30ddce4e949301d19cc26e727c27fa8
Mihael Stormrage <mihan.msn@hotmail.com>
2020-06-20 08:38:32 +0700

    Stage 3/4: Let’s count!

--- src/bot/SimpleBot.kt
+++ src/bot/SimpleBot.kt
@@ -15,9 +15,10 @@ fun main() {
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
     // reading all remainders
-    val mod3 = scanner.nextInt()
-    val mod5 = scanner.nextInt()
-    val mod7 = scanner.nextInt()
-
+    val (mod3, mod5, mod7) = List(3) { scanner.nextInt() }
     println("Your age is ${(mod3 * 70 + mod5 * 21 + mod7 * 15) % 105}; that's a good time to start programming!")
+
+    println("Now I will prove to you that I can count to any number you want.")
+    for (i in 0..scanner.nextInt()) { println("$i!") }
+    println("Completed, have a nice day!")
 }
